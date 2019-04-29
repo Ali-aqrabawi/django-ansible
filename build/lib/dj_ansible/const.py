220fd1696d68c52c186c613c2367ce62d027f274
Ali-aqrabawi <ali_aqrabawi@yahoo.com>
2019-04-29 21:29:01 +0300

    fixed models migration

---
+++ build/lib/dj_ansible/const.py
@@ -0,0 +1,23 @@
+ANSIBLE_CONNECTION_CHOICES = (
+    ('network_cli', 'network_cli'),
+    ('paramiko_ssh', 'paramiko'),
+    ('chroot', 'chroot'),
+    ('docker', 'docker'),
+    ('httpapi', 'httpapi'),
+    ('netconf', 'netconf'),
+    ('winrm', 'winrm')
+)
+
+ANSIBLE_NETWORK_OS_CHOICES = (
+    ('a10', 'a10'),
+    ('aci', 'aci'),
+    ('aireos', 'aireos'),
+    ('aos', 'aos'),
+    ('aruba', 'aruba'),
+    ('asa', 'asa'),
+    ('f5', 'f5'),
+    ('ios', 'ios'),
+    ('iosxr', 'iosxr'),
+    ('nxos', 'nxos')
+
+)
